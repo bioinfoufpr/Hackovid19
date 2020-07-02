467aae1f4945a8c185a6c607147c5aaef1240f45
Mariane Goncalves <mari.ti@gmail.com>
2020-07-02 16:32:27 -0300

    Fixing parameters to test predictions.

--- draft/load_model.py
+++ draft/load_model.py
@@ -9,16 +9,22 @@ Created on Mon Jun 29 10:51:29 2020
 from joblib import load
 import RXcovid as rx
 
-vars_prod = ['imgs_size', 'proj_path']
-locals_var =  locals()
+#vars_prod = ['imgs_size', 'proj_path']
+#locals_var =  locals()
 
-if not all([var in locals_var for var in vars_prod]):
-    imgs_size = int(input("Tamanho para resize da imagem: "))
-    proj_path = input("Caminho completo da matrix ortonormal para projeção: ")
+#if not all([var in locals_var for var in vars_prod]):
+#    imgs_size = int(input("Tamanho para resize da imagem: "))
+#    # Initial test = 10
+#    proj_path = input("Caminho completo da matrix ortonormal para projeção: ")
+#    # Initial test = 'mat_proj_3200_600.mat'
 
-path_img  = input("Nome da imagem  (nome completo): ") # where do the images comes from?
-model_path = input("Caminho e nome do modelo: ")
+path_img  = input("Nome da imagem  (nome completo): ")
+#model_path = input("Caminho e nome do modelo: ")
+# Initial test = 'output/RxCov10_model_02_07_2020_11_14_03.joblib'
 
+imgs_size = 10
+proj_path = 'mat_proj_3200_600.mat'
+model_path = 'output/RxCov10_model_02_07_2020_11_14_03.joblib'
 prefix = ''
 
 parameters = rx.TransfParam(2,imgs_size,'maxpool','none',3)
